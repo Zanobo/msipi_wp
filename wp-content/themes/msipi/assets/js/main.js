58d648c2dd7ccb5b1d11e1ad2dfc9db99077843f
Zach Risher <zrisher@yahoo.com>
2013-12-16 22:23:59 -0500

    enabled popup legal notices

---
+++ wp-content/themes/msipi/assets/js/main.js
@@ -0,0 +1,12 @@
+function legalpop(whichPage) {
+	var pagePath = 'http://www.fncinc.com/' + whichPage;
+	aWindow = window.open(pagePath, "FNCLegal", "status=no,toolbar=no,location=no,menu=no,scrollbars=yes,screenX=0,screenY=0,width=792,height=567");
+}
+
+$( "li.menu-terms-of-use a" ).click(function() {
+  legalpop('legal.aspx')
+});
+
+$( "li.menu-privacy-policy a" ).click(function() {
+  legalpop('legal.aspx#privacy')
+});

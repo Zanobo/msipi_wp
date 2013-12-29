f8617a83d8f6f4eaf5d6b091da6881bbe02dcecb
Zach Risher <zrisher@yahoo.com>
2013-12-28 21:53:25 -0500

    decreased top and bottom margin, moved main style back to assets, added nolan's em styling to style.css

--- wp-content/themes/msipi/assets/js/main.js
+++ wp-content/themes/msipi/assets/js/main.js
@@ -1,3 +1,4 @@
+/*
 function legalpop(whichPage) {
 	var pagePath = 'http://www.fncinc.com/' + whichPage;
 	aWindow = window.open(pagePath, "FNCLegal", "status=no,toolbar=no,location=no,menu=no,scrollbars=yes,screenX=0,screenY=0,width=792,height=567");
@@ -7,6 +8,7 @@ $( "li.menu-terms-of-use a" ).click(function() {
   legalpop('legal.aspx')
 });
 
-$( "li.menu-privacy-policy a" ).click(function() {
+$( "li.menu-privacy-statement a" ).click(function() {
   legalpop('legal.aspx#privacy')
 });
+*/

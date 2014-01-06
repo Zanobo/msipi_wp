ac4fe0d1fba8eeb0356601fe47a6e72856f1fbeb
Zach Risher <zach.risher@gmail.com>
2014-01-05 20:57:12 -0800

    attempting to override event templates

---
+++ wp-content/themes/msipi/single-event.php
@@ -0,0 +1,6 @@
+<header class="entry-header">
+  <h1 class="entry-title">HERES A TGEST OMG<?php echo $EM_Event->output('#_EVENTNAME'); ?></h1>
+</header>
+<div class="entry-content">
+  <p><?php echo $EM_Event->output('#_EVENTNOTES'); ?></p>
+</div>

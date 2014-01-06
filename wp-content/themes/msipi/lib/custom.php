de5049fd7572ff3119e545d5a9abe02d764347c8
Zach Risher <zach.risher@gmail.com>
2014-01-06 01:17:17 -0700

    succeeding in integrating attachments and events

--- wp-content/themes/msipi/lib/custom.php
+++ wp-content/themes/msipi/lib/custom.php
@@ -51,17 +51,17 @@ function msipi_add_em_attachments_placeholders($replace, $EM_Event, $result){
     switch( $result ){
        case '#_ATTACHPUB':
           $replace = '';
-            $attachments = new Attachments( 'msipi_attachments', $EM_Event->post_id );
-            if( $attachments->exist() ) :
-              $replace .= '<ul>';
-              while( $attachments->get() && ($attachments->field('restricted') == 0)  ) :
-                $replace .= '<li>' .
-                        '<a href="'.$attachments->url().'">'. $attachments->field('title')
-			.'</a></li>';
+          $attachments = new Attachments( 'msipi_attachments', $EM_Event->post_id );
+	  $attachments->search('0',array('fields' => array ('restricted')))
+          if( $attachments->exist() ) :
+            $replace .= '<ul>';
+            while( $attachments->get()  ) :
+              $replace .= '<li><a href="'.$attachments->url().'">'. $attachments->field('title')
+			.$attachments->field('restricted') . ($attachments->field('restricted') ==0 ? 'true' : 'false') .'</a></li>';
               endwhile;
-              $replace .= '</ul>';
-            endif;
+            $replace .= '</ul>';
           endif;
+          break;
 
 	case '#_ATTACHPRV':
           $replace = '';
